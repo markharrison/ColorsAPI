d1f5edba322077c030d5ecc659122ff7e2b5726a
markharrison <mark@harrison.ws>
2024-05-15 09:20:02 +0100

    Update .NET 8

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using System;
+using ColorsAPI.Services;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace ColorsAPI
 {
@@ -8,11 +10,110 @@ namespace ColorsAPI
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            ColorsService cs = new ColorsService(builder.Configuration);
+            builder.Services.AddSingleton(cs);
+
+            builder.Services.AddControllers();
+            builder.Services.AddCors();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Mark Harrison Colors API",
+                    Version = "v1",
+                    Description = "Colors API",
+                    TermsOfService = new Uri("https://github.com/markharrison/ColorsAPI/blob/master/LICENSE"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Mark Harrison",
+                        Email = "mark.colorsapi@harrison.ws",
+                        Url = new Uri("https://github.com/markharrison/ColorsAPI"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under MIT License",
+                        Url = new Uri("https://github.com/markharrison/ColorsAPI/blob/master/LICENSE"),
+                    }
+                }
+                );
+
+                c.EnableAnnotations();
+
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseCors(builder =>
+                      builder.WithOrigins("http://localhost")
+                              .AllowAnyOrigin()
+                              .AllowAnyHeader()
+                              .AllowAnyMethod());
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = (context) =>
+                {
+                    var headers = context.Context.Response.GetTypedHeaders();
+                    headers.CacheControl = new Microsoft.Net.Http.Headers.CacheControlHeaderValue
+                    {
+                        Public = true,
+                        MaxAge = TimeSpan.FromDays(1)
+                    };
+                }
+            });
+
+            app.UseSwagger(c =>
+            {
+                c.PreSerializeFilters.Add((swaggerDoc, httpRequest) =>
+                {
+                    var basePath = "/";
+                    var host = httpRequest.Host.Value;
+                    var scheme = (httpRequest.IsHttps || httpRequest.Headers["x-forwarded-proto"].ToString() == "https") ? "https" : "http";
+
+                    if (httpRequest.Headers["x-forwarded-host"].ToString() != "")
+                    {
+                        host = httpRequest.Headers["x-forwarded-host"].ToString() + ":" + httpRequest.Headers["x-forwarded-port"].ToString();
+                    }
+
+                    swaggerDoc.Servers = new List<OpenApiServer> { new OpenApiServer { Url = $"{scheme}://{host}{basePath}" } };
+
+                });
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mark Harrison Colors API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.MapGet("/appconfiginfo", async context =>
+            {
+                await context.Response.WriteAsync(cs.GetAppConfigInfo(context));
+            });
+
+            app.MapControllers();
+
+            app.Run();
+        }
     }
 }
--- README.md
+++ README.md
@@ -6,6 +6,8 @@ https://markharrison.io/
 
 19 April 2022 : Updated to .NET 6.0
 
+14 May 2024 : Updated to .NET 8.0
+
 Demo: https://colors-api.azurewebsites.net
 
 Also see: <https://github.com/markharrison/ColorsWeb>
@@ -15,6 +17,14 @@ Images: <https://github.com/markharrison/ColorsAPI/pkgs/container/colorsapi>
 - docker pull ghcr.io/markharrison/colorsapi:latest
 - docker pull ghcr.io/markharrison/colorsapi:blue
 - docker pull ghcr.io/markharrison/colorsapi:green
+
+## Configuration
+
+Environment variables / configuration 
+
+| Key          | Value     |  
+|--------------|-----------| 
+| ASPNETCORE_HTTP_PORTS | 8080  |  
 
 
 ## Package 
