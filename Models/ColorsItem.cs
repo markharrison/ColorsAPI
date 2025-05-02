19c6890149c7a577bec2bc0dcbe30b5524351d68
Mark Harrison <mark@harrison.ws>
2025-05-03 00:36:29 +0100

    Initialize Hexcode and Data properties in ColorsItem
    Updated the `Hexcode` and `Data` properties in the `ColorsItem` class to default to `string.Empty`. This change ensures that these properties are never null, enhancing the robustness of string data handling within the model.

--- Models/ColorsItem.cs
+++ Models/ColorsItem.cs
@@ -15,10 +15,9 @@ namespace ColorsAPI.Models
         public string Name { get; set; }
 
         [SwaggerSchema(Description = "Represents the hexadecimal code for the color. It is optional and can be an empty string.")]
-        public string Hexcode { get; set; }
+        public string Hexcode { get; set; } = string.Empty;
 
         [SwaggerSchema(Description = "Represents additional data or metadata related to the color. It is optional and can be an empty string.")]
-
-        public string Data { get; set; }
+        public string Data { get; set; } = string.Empty;
     }
 }
